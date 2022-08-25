607e140dd44d773c2fb5dc11960342e55004b77f
hngs91 <hngsmith91@gmail.com>
2022-08-25 12:38:36 +0100

    Updated

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,17 @@ import BlogList from "./BlogList";
 const Home = () => {
     const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
 
+    const audio = new Audio("/cyclophosphamide.mp3")
+
+    const handleClick = () => {
+        audio.play();
+    }
+
     return (
         <div className="home">
+            <div className="audio">
+                <button onClick={handleClick}>Audio</button>
+            </div>
             {error && <div>{error}</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
