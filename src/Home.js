119de8aa598e9cf3d82b10999f339a3a1cc18546
hngs91 <hngsmith91@gmail.com>
2022-08-25 09:57:26 +0100

    Added additional page, react-router-dom, json file

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,14 @@
+import useFetch from "./useFetch";
+import BlogList from "./BlogList";
+
 const Home = () => {
+    const { data: blogs, isPending, error } = useFetch('http://localhost:8000/blogs');
+
     return (
         <div className="home">
-            <h2>Homepage</h2>
+            {error && <div>{error}</div>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }
