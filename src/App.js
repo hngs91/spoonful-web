ef0872b70e6b5e256c4c30895354051d8a71ef11
hngs91 <hngsmith91@gmail.com>
2022-08-26 15:00:39 +0100

    removed unwanted pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import Create from './Create';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import BlogDetails from './BlogDetails';
 import NotFound from './NotFound';
@@ -15,10 +14,7 @@ function App() {
             <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/medicines/:id">
+            <Route path="/medicines/:drugname">
               <BlogDetails />
             </Route>
             <Route path="*">
